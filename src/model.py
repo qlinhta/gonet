dd0d4148ff9f0005b4afa69f5229ff155397b03e
Quyen Linh TA <qlinhta@outlook.com>
2023-11-12 15:00:59 +0100

    Update:: *.h5 legacy surpressing

--- src/model.py
+++ src/model.py
@@ -2,30 +2,34 @@ import tensorflow as tf
 from tensorflow.keras import layers, regularizers, Model
 
 
-class GoModel(Model):
+class GoModel:
     def __init__(self, planes, filters):
-        super(GoModel, self).__init__()
-        self.conv1 = layers.Conv2D(filters, 1, activation='relu', padding='same')
-        self.conv_blocks = [layers.Conv2D(filters, 3, activation='relu', padding='same') for _ in range(5)]
-        self.policy_head_conv = layers.Conv2D(1, 1, activation='relu', padding='same', use_bias=False,
-                                              kernel_regularizer=regularizers.l2(0.0001))
-        self.value_head_conv = layers.Conv2D(1, 1, activation='relu', padding='same', use_bias=False,
-                                             kernel_regularizer=regularizers.l2(0.0001))
-        self.flatten = layers.Flatten()
-        self.policy_head_activation = layers.Activation('softmax', name='policy')
-        self.value_head_dense1 = layers.Dense(50, activation='relu', kernel_regularizer=regularizers.l2(0.0001))
-        self.value_head_dense2 = layers.Dense(1, activation='sigmoid', name='value',
-                                              kernel_regularizer=regularizers.l2(0.0001))
+        # Define the input
+        input_layer = layers.Input(shape=(19, 19, planes), name='input')
 
-    def call(self, inputs):
-        x = self.conv1(inputs)
-        for conv in self.conv_blocks:
-            x = conv(x)
-        policy_head = self.policy_head_activation(self.flatten(self.policy_head_conv(x)))
-        value_head = self.value_head_dense2(self.value_head_dense1(self.flatten(self.value_head_conv(x))))
-        return {'policy': policy_head, 'value': value_head}
+        # Convolutional layers
+        x = layers.Conv2D(filters, 1, activation='relu', padding='same')(input_layer)
+        for _ in range(5):
+            x = layers.Conv2D(filters, 3, activation='relu', padding='same')(x)
+
+        # Policy head
+        policy_head = layers.Conv2D(1, 1, activation='relu', padding='same', use_bias=False,
+                                    kernel_regularizer=regularizers.l2(0.0001))(x)
+        policy_head = layers.Flatten()(policy_head)
+        policy_head = layers.Activation('softmax', name='policy')(policy_head)
+
+        # Value head
+        value_head = layers.Conv2D(1, 1, activation='relu', padding='same', use_bias=False,
+                                   kernel_regularizer=regularizers.l2(0.0001))(x)
+        value_head = layers.Flatten()(value_head)
+        value_head = layers.Dense(50, activation='relu', kernel_regularizer=regularizers.l2(0.0001))(value_head)
+        value_head = layers.Dense(1, activation='sigmoid', name='value')(value_head)
+
+        # Create the model
+        self.model = Model(inputs=input_layer, outputs=[policy_head, value_head])
+
+    def build(self):
+        return self.model
 
     def summary(self):
-        x = layers.Input(shape=(19, 19, 31))
-        model = Model(inputs=[x], outputs=self.call(x))
-        return model.summary()
+        return self.model.summary()
--- src/train.py
+++ src/train.py
@@ -6,7 +6,8 @@ import gc
 
 
 def train_model(epochs, batch_size, N, planes, moves, filters):
-    model = GoModel(planes, filters)
+    go = GoModel(planes, filters)
+    model = go.build()
     model.summary()
     model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.0005, momentum=0.9),
                   loss={'policy': 'categorical_crossentropy', 'value': 'binary_crossentropy'},
@@ -27,17 +28,18 @@ def train_model(epochs, batch_size, N, planes, moves, filters):
             golois.getValidation(input_data, policy, value, end)
             val = model.evaluate(input_data, [policy, value], verbose=0, batch_size=batch_size)
             print("Validation metrics:", val)
-            model.save('model_epoch_{}.h5'.format(epoch))
+            model.save('test.h5')
 
     return model
 
 
 if __name__ == '__main__':
-    epochs = 20
+    epochs = 5
     batch = 128
     N = 10000
     planes = 31
     moves = 361
-    filters = 32
+    filters = 16
 
     model = train_model(epochs, batch, N, planes, moves, filters)
+    model.save('test.h5')

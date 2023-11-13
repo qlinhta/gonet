d8c4956ddc1ca53ff750818b86ba52024a7d073c
Quyen Linh TA <qlinhta@outlook.com>
2023-11-13 23:41:48 +0100

    Update: LyonGo with MobileNetV2 @ failure

--- src/model.py
+++ src/model.py
@@ -1,29 +1,17 @@
 import tensorflow as tf
 from tensorflow.keras import layers, regularizers, Model
+from tensorflow.keras.optimizers.schedules import CosineDecay
 
 
-class GoModel:
+class ClassicGo:
     def __init__(self, planes, filters):
         # Define the input
         input_layer = layers.Input(shape=(19, 19, planes), name='input')
 
-        """# Convolutional layers
+        # Convolutional layers
         x = layers.Conv2D(filters, 1, activation='relu', padding='same')(input_layer)
         for _ in range(5):
-            x = layers.Conv2D(filters, 3, activation='relu', padding='same')(x)"""
-
-        x = layers.Conv2D(filters, 1, activation='relu', padding='same')(input_layer)
-
-        # Residual blocks // v2.batchnorm after conv2d
-        for _ in range(5):
-            residual = x
-            x = layers.Conv2D(filters, 3, padding='same')(x)
-            x = layers.BatchNormalization()(x)
-            x = layers.ReLU()(x)
-            x = layers.Conv2D(filters, 3, padding='same')(x)
-            x = layers.BatchNormalization()(x)
-            x = layers.add([x, residual])
-            x = layers.ReLU()(x)
+            x = layers.Conv2D(filters, 3, activation='relu', padding='same')(x)
 
         # Policy head
         policy_head = layers.Conv2D(1, 1, activation='relu', padding='same', use_bias=False,
@@ -46,3 +34,117 @@ class GoModel:
 
     def summary(self):
         return self.model.summary()
+
+
+class LyonGo:
+    def __init__(self, planes, filters, trunk, blocks):
+        self.input_layer = layers.Input(shape=(19, 19, planes), name='input')
+        self.filters = filters
+        self.trunk = trunk
+        self.blocks = blocks
+        self.model = self.build()
+
+    def se_block(self, input_tensor, filters, ratio=16):
+        # Squeeze and Excitation block implementation
+        se_shape = (1, 1, filters)
+        se = layers.GlobalAveragePooling2D()(input_tensor)
+        se = layers.Reshape(se_shape)(se)
+        se = layers.Dense(filters // ratio, activation='relu', use_bias=False)(se)
+        se = layers.Dense(filters, activation='sigmoid', use_bias=False)(se)
+        x = layers.multiply([input_tensor, se])
+        return x
+
+    def mobilenet(self, x, expand, squeeze):
+        x = layers.Conv2D(expand, (1, 1), kernel_regularizer=regularizers.l2(0.0001), use_bias=False)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('relu')(x)
+        x = layers.DepthwiseConv2D((3, 3), padding='same', kernel_regularizer=regularizers.l2(0.0001), use_bias=False)(
+            x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('relu')(x)
+        x = self.se_block(x, expand)
+        x = layers.Conv2D(squeeze, (1, 1), kernel_regularizer=regularizers.l2(0.0001), use_bias=False)(x)
+        x = layers.BatchNormalization()(x)
+        return layers.Add()([x, x])
+
+    def build(self):
+        x = layers.Conv2D(self.trunk, 1, padding='same', kernel_regularizer=regularizers.l2(0.0001))(self.input_layer)
+        x = layers.BatchNormalization()(x)
+        x = layers.ReLU()(x)
+        for _ in range(self.blocks):
+            x = self.mobilenet(x, self.filters, self.trunk)
+
+        policy_head = layers.Conv2D(1, 1, activation='relu', padding='same', use_bias=False,
+                                    kernel_regularizer=regularizers.l2(0.0001))(x)
+        policy_head = layers.Flatten()(policy_head)
+        policy_head = layers.Activation('softmax', name='policy')(policy_head)
+
+        value_head = layers.GlobalAveragePooling2D()(x)
+        value_head = layers.Dense(50, activation='relu', kernel_regularizer=regularizers.l2(0.0001))(value_head)
+        value_head = layers.Dense(1, activation='sigmoid', name='value', kernel_regularizer=regularizers.l2(0.0001))(
+            value_head)
+
+        model = Model(inputs=self.input_layer, outputs=[policy_head, value_head])
+        return model
+
+    def summary(self):
+        return self.model.summary()
+
+    def save_model(self, file_path):
+        self.model.save(file_path)
+
+
+class ParisGo:
+    def __init__(self, planes, filters, steps, initial_learning_rate):
+        self.input_layer = layers.Input(shape=(19, 19, planes), name='input')
+        self.filters = filters
+        self.planes = planes
+        self.lr_schedule = CosineDecay(initial_learning_rate, steps)
+
+        self.model = self.build()
+
+    def build(self):
+        x = self.mixnet_block(self.input_layer, self.filters)
+
+        for _ in range(4):
+            x = self.mixnet_block(x, self.filters)
+
+        policy_head = self.create_policy_head(x)
+        value_head = self.create_value_head(x)
+
+        model = Model(inputs=self.input_layer, outputs=[policy_head, value_head])
+        return model
+
+    def mixnet_block(self, input_tensor, filters):
+        # BatchNorm
+        branch_a = layers.Conv2D(filters, 1, activation='swish', padding='same')(input_tensor)
+        branch_a = layers.BatchNormalization()(branch_a)
+        branch_b = layers.Conv2D(filters, (3, 3), activation='swish', padding='same')(input_tensor)
+        branch_b = layers.BatchNormalization()(branch_b)
+        branch_c = layers.Conv2D(filters, (5, 5), activation='swish', padding='same')(input_tensor)
+        branch_c = layers.BatchNormalization()(branch_c)
+
+        mixed = layers.Concatenate()([branch_a, branch_b, branch_c])
+        mixed = layers.Conv2D(filters, 1, activation='swish', padding='same')(mixed)
+        return mixed
+
+    def create_policy_head(self, x):
+        policy_head = layers.Conv2D(1, 1, activation='swish', padding='same', use_bias=False,
+                                    kernel_regularizer=regularizers.l2(0.0001))(x)
+        policy_head = layers.Flatten()(policy_head)
+        policy_head = layers.Activation('softmax', name='policy')(policy_head)
+        return policy_head
+
+    def create_value_head(self, x):
+        value_head = layers.Conv2D(1, 1, activation='swish', padding='same', use_bias=False,
+                                   kernel_regularizer=regularizers.l2(0.0001))(x)
+        value_head = layers.Flatten()(value_head)
+        value_head = layers.Dense(50, activation='swish', kernel_regularizer=regularizers.l2(0.0001))(value_head)
+        value_head = layers.Dense(1, activation='sigmoid', name='value')(value_head)
+        return value_head
+
+    def summary(self):
+        return self.model.summary()
+
+    def save_model(self, file_path):
+        self.model.save(file_path)
--- src/train.py
+++ src/train.py
@@ -1,12 +1,13 @@
 import tensorflow as tf
 import json
 import argparse
-from model import GoModel
+from model import ClassicGo, ParisGo, LyonGo
 from dataloader import load_data
 import golois
 import gc
 import os
 from datetime import datetime
+from prettytable import PrettyTable
 
 """def train_model(epochs, batch_size, N, planes, moves, filters):
     go = GoModel(planes, filters)
@@ -57,12 +58,15 @@ class CustomCallback(tf.keras.callbacks.Callback):
             golois.getValidation(self.input_data, self.policy, self.value, self.end)
             val = self.model.evaluate(self.input_data, [self.policy, self.value], verbose=0, batch_size=self.batch_size)
             print("Validation metrics:", val)
-            self.model.save(f'models/adam_model_{val[3]:.2f}.h5')
+            # self.model.save(f'models/ParisGo_MixNet_Cosin_Swish_{val[3]:.2f}.h5')
+            self.model.save(f'models/LyonGo_{val[3]:.2f}.h5')
 
 
 def train_model(epochs, batch_size, N, planes, moves, filters):
-    go = GoModel(planes, filters)
-    model = go.build()
+    classic = ClassicGo(planes, filters)
+    lyon = LyonGo(planes, filters, 128, 5)
+    paris = ParisGo(planes, filters, 1000, 0.005)
+    model = lyon.build()
     model.summary()
 
     """lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
@@ -75,10 +79,15 @@ def train_model(epochs, batch_size, N, planes, moves, filters):
                   loss_weights={'policy': 1.0, 'value': 1.0},
                   metrics={'policy': 'categorical_accuracy', 'value': 'mse'})"""
 
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.005),
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0005),
                   loss={'policy': 'categorical_crossentropy', 'value': 'binary_crossentropy'},
                   loss_weights={'policy': 1.0, 'value': 1.0},
                   metrics={'policy': 'categorical_accuracy', 'value': 'mse'})
+
+    """model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=go.lr_schedule),
+                  loss={'policy': 'categorical_crossentropy', 'value': 'binary_crossentropy'},
+                  loss_weights={'policy': 1.0, 'value': 1.0},
+                  metrics={'policy': 'categorical_accuracy', 'value': 'mse'})"""
 
     input_data, policy, value, end, groups = load_data(N, planes, moves)
     print("getValidation", flush=True)
@@ -106,11 +115,21 @@ if __name__ == '__main__':
     with open(args.config, 'r') as f:
         config = json.load(f)
 
-    epochs = config.get('epochs', 200)
-    batch = config.get('batch', 256)
+    epochs = config.get('epochs', 100)
+    batch = config.get('batch', 128)
     N = config.get('N', 10000)
     planes = config.get('planes', 31)
     moves = config.get('moves', 361)
-    filters = config.get('filters', 16)
+    filters = config.get('filters', 32)
+
+    table = PrettyTable()
+    table.field_names = ["Parameter/Hyperparameter", "Value"]
+    table.add_row(["Epochs", epochs])
+    table.add_row(["Batch Size", batch])
+    table.add_row(["N", N])
+    table.add_row(["Planes", planes])
+    table.add_row(["Moves", moves])
+    table.add_row(["Filters", filters])
+    print(table)
 
     model = train_model(epochs, batch, N, planes, moves, filters)

1d6375e7754e8a3aa80fdc98bcf2992c41275365
Quyen Linh TA <qlinhta@outlook.com>
2023-11-14 22:31:00 +0000

    Update:: Colab's session @ successful -h

--- src/train.py
+++ src/train.py
@@ -32,45 +32,43 @@ class CustomCallback(tf.keras.callbacks.Callback):
             val = self.model.evaluate(self.input_data, [self.policy, self.value], verbose=0, batch_size=self.batch_size)
             print("Validation metrics:", val)
             # self.model.save(f'models/ParisGo_MixNet_Cosin_Swish_128_0.005_{val[3]:.2f}.h5')
-            self.model.save(f'models/LyonGo_10K_128_5_0.00001_{val[3]:.2f}.h5')
+            self.model.save(f'models/LyonGo_10K_128_5_0.00001_64_{val[3]:.2f}.h5')
 
 
-def train_model(epochs, batch_size, N, planes, moves, filters):
-    classic = ClassicGo(planes, filters)
-    lyon = LyonGo(planes, filters, 128, 5)
-    paris = ParisGo(planes, filters, 1000, 0.005)
-    model = lyon.build()
+def train_model(model_name, epochs, batch_size, N, planes, moves, filters):
+    if model_name == "LyonGo":
+        model = LyonGo(planes, filters, 128, 5).build()
+    elif model_name == "ClassicGo":
+        model = ClassicGo(planes, filters).build()
+    elif model_name == "ParisGo":
+        model = ParisGo(planes, filters, 1000, 0.005).build()
+    else:
+        raise ValueError(f"No model found with the name '{model_name}'.")
+
     model.summary()
 
-    """lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-        initial_learning_rate=0.005,
-        decay_steps=4000,
-        decay_rate=0.9)"""
+    if model_name == "LyonGo":
+        learning_rate = 0.00001
+        optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    elif model_name == "ClassicGo":
+        lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+            initial_learning_rate=0.005, decay_steps=4000, decay_rate=0.9)
+        optimizer = tf.keras.optimizers.SGD(learning_rate=lr_schedule, momentum=0.9)
+    elif model_name == "ParisGo":
+        optimizer = tf.keras.optimizers.Adam(learning_rate=model.lr_schedule)
 
-    """model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=lr_schedule, momentum=0.9),
-                  loss={'policy': 'categorical_crossentropy', 'value': 'binary_crossentropy'},
-                  loss_weights={'policy': 1.0, 'value': 1.0},
-                  metrics={'policy': 'categorical_accuracy', 'value': 'mse'})"""
-
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.00001),
+    model.compile(optimizer=optimizer,
                   loss={'policy': 'categorical_crossentropy', 'value': 'binary_crossentropy'},
                   loss_weights={'policy': 1.0, 'value': 1.0},
                   metrics={'policy': 'categorical_accuracy', 'value': 'mse'})
-
-    """model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=paris.lr_schedule),
-                  loss={'policy': 'categorical_crossentropy', 'value': 'binary_crossentropy'},
-                  loss_weights={'policy': 1.0, 'value': 1.0},
-                  metrics={'policy': 'categorical_accuracy', 'value': 'mse'})"""
 
     input_data, policy, value, end, groups = load_data(N, planes, moves)
     print("getValidation", flush=True)
     golois.getValidation(input_data, policy, value, end)
 
-    # TensorBoard callback
-    log_dir = os.path.join("logs", "fit", datetime.now().strftime("%Y%m%d-%H%M%S"))
+    log_dir = os.path.join("logs", "fit", f"{model_name}_{datetime.now().strftime('%Y%m%d-%H%M%S')}")
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1, update_freq='epoch')
 
-    # Custom callback
     custom_callback = CustomCallback(input_data, policy, value, end, groups, N, batch_size)
 
     model.fit(input_data, {'policy': policy, 'value': value}, epochs=epochs, batch_size=batch_size,
@@ -94,9 +92,11 @@ if __name__ == '__main__':
     planes = config.get('planes', 31)
     moves = config.get('moves', 361)
     filters = config.get('filters', 32)
+    model_name = config.get('model_name', "ClassicGo")
 
     table = PrettyTable()
     table.field_names = ["Parameter/Hyperparameter", "Value"]
+    table.add_row(["Model", model_name])
     table.add_row(["Epochs", epochs])
     table.add_row(["Batch Size", batch])
     table.add_row(["N", N])
@@ -105,4 +105,4 @@ if __name__ == '__main__':
     table.add_row(["Filters", filters])
     print(table)
 
-    model = train_model(epochs, batch, N, planes, moves, filters)
+    model = train_model(model_name, epochs, batch, N, planes, moves, filters)

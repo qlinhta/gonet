e9255dac2903fedc231ed4c4e0fd6acd3dc7959b
Quyen Linh TA <qlinhta@outlook.com>
2023-11-15 08:45:58 +0000

    Update:: Colab's session @ successful --h --config bundle 5

--- src/train.py
+++ src/train.py
@@ -32,7 +32,7 @@ class CustomCallback(tf.keras.callbacks.Callback):
             val = self.model.evaluate(self.input_data, [self.policy, self.value], verbose=0, batch_size=self.batch_size)
             print("Validation metrics:", val)
             # self.model.save(f'models/ParisGo_MixNet_Cosin_Swish_128_0.005_{val[3]:.2f}.h5')
-            self.model.save(f'models/LyonGo_10K_128_5_0.0001_64_{val[3]:.2f}.h5')
+            self.model.save(f'models/LyonGo_10K_128_5_annealing_64_{val[3]:.2f}.h5')
 
 
 def train_model(model_name, epochs, batch_size, N, planes, moves, filters):
@@ -49,7 +49,7 @@ def train_model(model_name, epochs, batch_size, N, planes, moves, filters):
 
     if model_name == "LyonGo":
         boundaries = [100, 150]
-        values = [0.005, 0.0005, 0.00005]
+        values = [0.0005, 0.0001, 0.00005]
         lr_schedule = tf.keras.optimizers.schedules.PiecewiseConstantDecay(boundaries, values)
         optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
     elif model_name == "ClassicGo":

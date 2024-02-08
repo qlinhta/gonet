be4c4f2684d7459b53b273b0c6976fb2a3784f83
Quyen Linh TA <60761870+qlinhta@users.noreply.github.com>
2024-02-08 14:49:24 +0100

    Update :: Suppose that solution is correct

--- src/ConvNext.py
+++ src/ConvNext.py
@@ -7,7 +7,6 @@ from prettytable import PrettyTable
 from tensorflow import keras
 import gc
 from tensorflow.keras.optimizers.schedules import CosineDecay
-from keras import ops
 
 plt.style.use('default')
 plt.rc('text', usetex=False)
@@ -26,20 +25,16 @@ N = 10000
 epochs = 200
 batch = 128
 learning_rate = 0.001
-num_heads = 4
-num_transformer_blocks = 4
-d_model = 32
 dropout_rate = 0
 decay_steps = N / batch * epochs
-num_res_blocks = 3
-res_filters = 32
+blocks = 4
+filters = 32
 
 table = PrettyTable()
-table.field_names = ["Transformer Blocks", "Head", "Epoch", "Batch", "N", "Planes", "Moves", "D-Model", "Learning Rate",
+table.field_names = ["Epoch", "Batch", "N", "Planes", "Moves", "D-Model", "Learning Rate",
                      "Dropout Rate", "Decay Steps"]
 table.add_row(
-    [num_transformer_blocks, num_heads, epochs, batch, N, planes, moves, d_model, learning_rate, dropout_rate,
-     decay_steps])
+    [epochs, batch, N, planes, moves, filters, learning_rate, dropout_rate, decay_steps])
 print(table)
 
 train_losses = []
@@ -65,81 +60,17 @@ groups = groups.astype('float32')
 print("getValidation", flush=True)
 golois.getValidation(input_data, policy, value, end)
 
-
-def mlp(x, hidden_units, dropout_rate):
-    for units in hidden_units:
-        x = layers.Dense(units, activation=keras.activations.gelu)(x)
-        x = layers.Dropout(dropout_rate)(x)
-    return x
-
-
-class Patches(layers.Layer):
-    def __init__(self, patch_size):
-        super().__init__()
-        self.patch_size = patch_size
-
-    def call(self, images):
-        input_shape = ops.shape(images)
-        batch_size = input_shape[0]
-        height = input_shape[1]
-        width = input_shape[2]
-        channels = input_shape[3]
-        num_patches_h = height // self.patch_size
-        num_patches_w = width // self.patch_size
-        patches = keras.ops.image.extract_patches(images, size=self.patch_size)
-        patches = ops.reshape(
-            patches,
-            (
-                batch_size,
-                num_patches_h * num_patches_w,
-                self.patch_size * self.patch_size * channels,
-            ),
-        )
-        return patches
-
-    def get_config(self):
-        config = super().get_config()
-        config.update({"patch_size": self.patch_size})
-        return config
-
-
-
-class PatchEncoder(layers.Layer):
-    def __init__(self, num_patches, projection_dim):
-        super().__init__()
-        self.num_patches = num_patches
-        self.projection = layers.Dense(units=projection_dim)
-        self.position_embedding = layers.Embedding(
-            input_dim=num_patches, output_dim=projection_dim
-        )
-
-    def call(self, patch):
-        positions = ops.expand_dims(
-            ops.arange(start=0, stop=self.num_patches, step=1), axis=0
-        )
-        projected_patches = self.projection(patch)
-        encoded = projected_patches + self.position_embedding(positions)
-        return encoded
-
-    def get_config(self):
-        config = super().get_config()
-        config.update({"num_patches": self.num_patches})
-        return config
-
-
 input = layers.Input(shape=(19, 19, planes), name='board')
-patch_size = 6
-patches = Patches(patch_size)(input)
-encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
-for _ in range(transformer_layers):
-    x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
-    attention_output = layers.MultiHeadAttention(num_heads=num_heads, key_dim=projection_dim, dropout=0.1)(x1, x1)
-    x2 = layers.Add()([attention_output, encoded_patches])
-    x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
-    x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
-    encoded_patches = layers.Add()([x3, x2])    
-representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)  
-
+x = layers.Conv2D(filters, 3, activation='relu', padding='same')(input)
+for i in range(blocks):
+    x1 = tf.keras.layers.DepthwiseConv2D((7, 7), padding='same', use_bias=False)(x)
+    x1 = layers.LayerNormalization()(x1)
+    x1 = layers.Conv2D(4 * filters, 1, padding='same', activation='gelu')(x1)
+    x1 = layers.Conv2D(filters, 1, padding='same')(x1)
+    x = layers.add([x1, x])
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = layers.Activation('gelu')(x)
+    x = layers.SpatialDropout2D(dropout_rate)(x)
 policy_head = layers.Conv2D(1, 1, activation='relu', padding='same', use_bias=False,
                             kernel_regularizer=regularizers.l2(0.0001))(x)
 policy_head = layers.Flatten()(policy_head)
@@ -153,8 +84,8 @@ value_head = layers.Dense(1, activation='sigmoid', name='value', kernel_regulari
 model = keras.Model(inputs=input, outputs=[policy_head, value_head])
 model.summary()
 
-# lr_schedule = CosineDecay(initial_learning_rate=learning_rate, decay_steps=decay_steps)
-optimizer = tf.keras.optimizers.AdamW(learning_rate=learning_rate, weight_decay=0.0001)
+lr_schedule = CosineDecay(initial_learning_rate=learning_rate, decay_steps=decay_steps)
+optimizer = tf.keras.optimizers.AdamW(learning_rate=lr_schedule, weight_decay=0.0001)
 
 model.compile(optimizer=optimizer,
               loss={'policy': 'categorical_crossentropy', 'value': 'binary_crossentropy'},
@@ -179,8 +110,7 @@ for i in range(1, epochs + 1):
         train_acc.append(history.history['policy_categorical_accuracy'][0])
         val_acc.append(val[3])
         model.save(
-            f"models/TranxGo_{i}_{num_transformer_blocks}_{num_heads}_{epochs}_{batch}_{N}_{planes}_{moves}_{d_model}_{learning_rate}_{dropout_rate}_{decay_steps}_val_{val[3]:.2f}.h5")
-
+            f"models/ConvNext_{i}_{epochs}_{batch}_{learning_rate}_{N}_{filters}_val_{val[3]:.2f}.h5")
 
         fig, axs = plt.subplots(1, 2, figsize=(10, 5))
         axs[0].plot(train_losses, label='Train loss', color='grey', linestyle='dashed', linewidth=1, marker='o',
@@ -201,5 +131,5 @@ for i in range(1, epochs + 1):
         axs[1].set(xlabel='Every #10 Epoch')
         plt.tight_layout()
         plt.savefig(
-            f"figures/TranxGo_{i}_{num_transformer_blocks}_{num_heads}_{epochs}_{batch}_{N}_{planes}_{moves}_{d_model}_{learning_rate}_{dropout_rate}_{decay_steps}_val_{val[3]:.2f}.pdf")
+            f"figures/ConvNext_{i}_{epochs}_{batch}_{learning_rate}_{N}_{filters}_val_{val[3]:.2f}.pdf")
         plt.close()

f811ececa2f5bb538dd54fbb3482cb2dea2890ad
Quyen Linh TA <60761870+qlinhta@users.noreply.github.com>
2024-02-10 17:22:13 +0100

    Update :: Suppose that solution is correct

--- prepare.py
+++ prepare.py
@@ -1,22 +1,45 @@
+import os
 import requests
 import zipfile
-import os
 from io import BytesIO
+import subprocess  # For executing git commands
+
+
+def download_and_extract_zip(url, extract_to='./data'):
+    print(f"Downloading zip file from {url}")
+    response = requests.get(url)
+    with zipfile.ZipFile(BytesIO(response.content)) as zip_file:
+        print(f'Extracting files to {extract_to}')
+        zip_file.extractall(extract_to)
+    print("Extraction complete!")
+
+
+def move_file(source, destination):
+    if not os.path.exists(os.path.dirname(destination)):
+        print(f"Creating directory for {destination}...")
+        os.makedirs(os.path.dirname(destination))
+
+    print(f"Moving {source} to {destination}...")
+    os.rename(source, destination)
+    print(f"File '{os.path.basename(destination)}' has been moved to '{destination}'.")
+
+
+def setup_git_config(email, name):
+    print("Configuring git user email and name...")
+    subprocess.run(['git', 'config', '--global', 'user.email', email], check=True)
+    subprocess.run(['git', 'config', '--global', 'user.name', name], check=True)
+    print("Git configuration completed.")
+
 
 file_url = "https://www.lamsade.dauphine.fr/~cazenave/project2022.zip"
-
-response = requests.get(file_url)
-zip_file = zipfile.ZipFile(BytesIO(response.content))
-
-zip_file.extractall("./data")
-
 source_file_path = "./data/games.data"
 destination_directory = "./"
-
-if not os.path.exists(destination_directory):
-    os.makedirs(destination_directory)
-
 destination_file_path = os.path.join(destination_directory, "games.data")
-os.rename(source_file_path, destination_file_path)
 
-print(f"File 'games.data' has been copied to '{destination_file_path}'.")
+download_and_extract_zip(file_url)
+move_file(source_file_path, destination_file_path)
+
+email = "qlinhta@outlook.com"
+name = "Quyen Linh TA"
+setup_git_config(email, name)
+print("Git configuration completed.")

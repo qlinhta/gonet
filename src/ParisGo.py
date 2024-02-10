4aff0b24195811b21490c315c699fed813a32352
Quyen Linh TA <60761870+qlinhta@users.noreply.github.com>
2024-02-10 14:17:16 +0100

    Update :: Suppose that solution is correct

--- src/ParisGo.py
+++ src/ParisGo.py
@@ -9,27 +9,20 @@ from prettytable import PrettyTable
 import golois
 import matplotlib.pyplot as plt
 
-
 planes = 31
 moves = 361
 N = 10000
-epochs = 350
+epochs = 100
 batch = 128
 filters = 16
 dropout_rate = 0
-learning_rate = 0.005
-nblocks = 5
+learning_rate = 0.0005
 decay_steps = N / batch * epochs
 
 train_losses = []
 val_losses = []
 train_acc = []
 val_acc = []
-
-table = PrettyTable()
-table.field_names = ["Epoch", "Batch", "N", "Planes", "Moves", "Filters", "Learning Rate", "Dropout Rate", "Decay Steps"]
-table.add_row([epochs, batch, N, planes, moves, filters, learning_rate, dropout_rate, decay_steps])
-print(table)
 
 input_data = np.random.randint(2, size=(N, 19, 19, planes))
 input_data = input_data.astype('float32')
@@ -50,42 +43,82 @@ print("getValidation", flush=True)
 golois.getValidation(input_data, policy, value, end)
 
 
-def mixnet_block(input_tensor, filters):
-    branch_a = layers.Conv2D(filters, 1, activation='swish', padding='same')(input_tensor)
-    branch_a = layers.BatchNormalization()(branch_a)
-    branch_b = layers.Conv2D(filters, (3, 3), activation='swish', padding='same')(input_tensor)
-    branch_b = layers.BatchNormalization()(branch_b)
-    branch_c = layers.Conv2D(filters, (5, 5), activation='swish', padding='same')(input_tensor)
-    branch_c = layers.BatchNormalization()(branch_c)
-    mixed = layers.Concatenate()([branch_a, branch_b, branch_c])
-    mixed = layers.Conv2D(filters, 1, activation='swish', padding='same')(mixed)
-    return mixed
+def SEBlock(tensor, filters, ratio=16):
+    se_shape = (1, 1, filters)
+    se = layers.GlobalAveragePooling2D()(tensor)
+    se = layers.Reshape(se_shape)(se)
+    se = layers.Dense(filters // ratio, activation='relu', use_bias=False)(se)
+    se = layers.Dense(filters, activation='sigmoid', use_bias=False)(se)
+    return layers.Multiply()([tensor, se])
 
 
-input = keras.Input(shape=(19, 19, planes), name='board')
-x = mixnet_block(input, filters)
-x = layers.Dropout(dropout_rate)(x)
+def MixConv(tensor, filters, **kwargs):
+    groups = len(filters)
+    split_tensors = tf.split(tensor, groups, axis=-1)
+    mixed_convs = []
+    for i, (split, f) in enumerate(zip(split_tensors, filters)):
+        conv = layers.DepthwiseConv2D(kernel_size=2 * (i + 1) + 1, strides=1, padding='same', **kwargs)(split)
+        conv = layers.BatchNormalization()(conv)
+        conv = layers.Activation('swish')(conv)
+        mixed_convs.append(conv)
+    return tf.concat(mixed_convs, axis=-1)
 
-for _ in range(nblocks):
-    x = mixnet_block(x, filters)
-    x = layers.Dropout(dropout_rate)(x)
+
+def DepthwiseConvBlock(tensor, strides):
+    conv = layers.DepthwiseConv2D(3, strides=strides, padding='same' if strides == 1 else 'valid', use_bias=False)(
+        tensor)
+    conv = layers.BatchNormalization()(conv)
+    return layers.Activation('swish')(conv)
+
+
+def PointwiseConvBlock(tensor, filters, linear=False):
+    conv = layers.Conv2D(filters=filters, kernel_size=(1, 1), strides=(1, 1), padding='same', use_bias=False)(tensor)
+    conv = layers.BatchNormalization()(conv)
+    if not linear:
+        conv = layers.Activation('swish')(conv)
+    return conv
+
+
+def ConvBlock(tensor):
+    conv = layers.Conv2D(filters=32, kernel_size=(1, 1), use_bias=False)(tensor)
+    conv = layers.BatchNormalization()(conv)
+    return layers.Activation('swish')(conv)
+
+
+def InvertedResidualBlock(tensor, strides, filters, expansion):
+    expanded = PointwiseConvBlock(tensor, filters=expansion * filters, linear=False)
+    mixed = MixConv(expanded, [filters / 4] * 4)
+    pointwise = PointwiseConvBlock(mixed, filters=filters, linear=True)
+    if strides == 1 and tensor.shape[-1] == filters:
+        pointwise = SEBlock(pointwise, filters)
+        return layers.Add()([pointwise, tensor])
+    return pointwise
+
+
+def BottleneckBlock(tensor, strides, channels, expansion, repeats):
+    x = InvertedResidualBlock(tensor, strides=strides, filters=channels, expansion=expansion)
+    for _ in range(repeats - 1):
+        x = InvertedResidualBlock(x, strides=1, filters=channels, expansion=expansion)
+    return x
+
+
+inputs = keras.Input(shape=(19, 19, planes), name='board')
+x = ConvBlock(inputs)
+x = BottleneckBlock(x, strides=1, channels=16, expansion=1, repeats=1)
+x = BottleneckBlock(x, strides=1, channels=24, expansion=2, repeats=2)
+x = BottleneckBlock(x, strides=1, channels=32, expansion=2, repeats=2)
+x = BottleneckBlock(x, strides=1, channels=64, expansion=2, repeats=3)
+x = PointwiseConvBlock(x, filters=64, linear=False)
 
 policy_head = layers.Conv2D(1, 1, activation='swish', padding='same', use_bias=False,
                             kernel_regularizer=regularizers.l2(0.0001))(x)
 policy_head = layers.Flatten()(policy_head)
-policy_head = layers.Dropout(dropout_rate)(policy_head)
 policy_head = layers.Activation('softmax', name='policy')(policy_head)
-
-value_head = layers.Conv2D(1, 1, activation='swish', padding='same', use_bias=False,
-                           kernel_regularizer=regularizers.l2(0.0001))(x)
-value_head = layers.Flatten()(value_head)
-value_head = layers.Dropout(dropout_rate)(value_head)
+value_head = layers.GlobalAveragePooling2D()(x)
 value_head = layers.Dense(50, activation='swish', kernel_regularizer=regularizers.l2(0.0001))(value_head)
-value_head = layers.Dense(1, activation='sigmoid', name='value')(value_head)
+value_head = layers.Dense(1, activation='sigmoid', name='value', kernel_regularizer=regularizers.l2(0.0001))(value_head)
 
-model = keras.Model(inputs=input, outputs=[policy_head, value_head])
-
-model.summary()
+model = keras.Model(inputs=inputs, outputs=[policy_head, value_head])
 
 lr_schedule = CosineDecay(initial_learning_rate=learning_rate, decay_steps=decay_steps)
 optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
@@ -94,6 +127,8 @@ model.compile(optimizer=optimizer,
               loss={'policy': 'categorical_crossentropy', 'value': 'binary_crossentropy'},
               loss_weights={'policy': 1.0, 'value': 1.0},
               metrics={'policy': 'categorical_accuracy', 'value': 'mse'})
+
+model.summary()
 
 for i in range(1, epochs + 1):
     print('epoch ' + str(i))
@@ -108,12 +143,8 @@ for i in range(1, epochs + 1):
         val = model.evaluate(input_data,
                              [policy, value], verbose=0, batch_size=batch)
         print("val =", val)
-        train_losses.append(history.history['policy_loss'][0])
-        val_losses.append(val[1])
-        train_acc.append(history.history['policy_categorical_accuracy'][0])
-        val_acc.append(val[3])
         model.save(
-            f"models/ParisGo_{i}_{epochs}_{batch}_{learning_rate}_{N}_{filters}_{dropout_rate}_val_{val[3]:.2f}.h5")
+            f"models/ParisGo_{i}_{epochs}_{batch}_{learning_rate}_{N}_{dropout_rate}_val_{val[3]:.2f}.h5")
 
         fig, axs = plt.subplots(1, 2, figsize=(10, 5))
         axs[0].plot(train_losses, label='Train loss', color='grey', linestyle='dashed', linewidth=1, marker='o',
@@ -134,5 +165,5 @@ for i in range(1, epochs + 1):
         axs[1].set(xlabel='Every #10 Epoch')
         plt.tight_layout()
         plt.savefig(
-            f"figures/ParisGo_{epochs}_{batch}_{learning_rate}_{N}_{filters}_{dropout_rate}_val_{val[3]:.2f}.pdf")
+            f"figures/ParisGo_{i}_{epochs}_{batch}_{learning_rate}_{N}_val_{val[3]:.2f}.pdf")
         plt.close()
